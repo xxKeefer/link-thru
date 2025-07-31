b0e7d6fbf709b76a41e718a24a5954ff8b24379f
xxKeefer <xxkeefer.code@gmail.com>
2025-08-01 05:46:37 +1000

    feat: :lipstick: add tailwind and fonts to fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="flex flex-col gap-2">
+      <h1 className="text-4xl">Test</h1>
+      <p className="font-display">Test 1 2 3</p>
+      <p className="font-emoji">🧙‍♂️</p>
+      <p className="font-sans">Lorem Ipsum</p>
+      <p className="font-sans font-black">Lorem Ipsum Fat</p>
+      <p className="font-sans font-light">Lorem Ipsum thin</p>
+      <p className="font-mono">Arigato Mr. Roboto</p>
+    </main>
+  );
 }
 
-export default App
+export default App;
